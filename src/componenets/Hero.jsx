65783fd204254a2f1e0b7e0ec0a39917a06aed18
Ruchali Aery <112783914+ruchali1@users.noreply.github.com>
2024-12-14 09:55:43 -0700

    No REGISTER

--- src/componenets/Hero.jsx
+++ src/componenets/Hero.jsx
@@ -7,17 +7,17 @@ const Hero = () => {
         <h2 className="text-5xl font-bold mb-4">
           HACKED
           <br />
-          <span className="text-pink-500">BETA</span>
+          <span className="text-pink-500">2025</span>
           <br />
         </h2>
         <p className="text-lg mb-8">
-        Hackathons are for designing, creating, and building awesome projects. HackED Beta is the premiere annual 24-hour hackathon from the Computer Engineering Club of the University of Alberta.
+        Hackathons are for designing, creating, and building awesome projects. This year HackED is a 48-hour hackathon from the Computer Engineering Club of the University of Alberta.
 
         </p>
         <button className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded-full">
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSewv6xgpW_XlPPJ5bN2yMBpyniYIsH_ouZE1RlgKKe6EN2tIg/viewform" className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded-full">
-  REGISTER TODAY
-        </a>
+{/*         <a href="https://docs.google.com/forms/d/e/1FAIpQLSewv6xgpW_XlPPJ5bN2yMBpyniYIsH_ouZE1RlgKKe6EN2tIg/viewform" className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded-full"> */}
+  REGISTRATION OPEN SOON
+{/*         </a> */}
         </button>
       </div>
     </section>
